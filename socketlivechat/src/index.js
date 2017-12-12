d0c06676076f9ee120268b26174e53cd1a87bcb3
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-12 11:37:45 +0800

    socket livechat testing learning now

--- socketlivechat/src/index.js
+++ socketlivechat/src/index.js
@@ -7,13 +7,27 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/build/index.html')
 })
 
-server.listen(process.env.PORT)
+server.listen(process.env.PORT, () => {
+    console.log('livechat server listening on port ' + process.env.PORT)
+})
 
-// socket io connection
-io.on('connection', function (socket) {
-    console.log('a user has connected')
-    socket.emit('news', { hello: 'world' })
-    socket.on('my other event', function (data) {
-        console.log(data)
+// each live chat projects is one room
+// room name will be the UUID of the livechat project
+var totalrooms = ['1', '2', '3']
+
+io.on('connection', (socket) => {
+
+    console.log('a user has connected, checked which room this user is supposed to be in?')
+
+    let roomname = 'm8nySihGhfPK37Wj5dTNkUHSnkMdaVjVJjFGvZMp462pbqJAB'
+
+    socket.join(roomname, () => {
+        let rooms = Object.keys(socket.rooms)
+        console.log(rooms) // [ 'room name', 'socket id' ]
     })
+
+    io.emit('listenpls', 'hello ' + socket.id)
+
+    socket.to(roomname).emit('an event', { some: 'data' })
+
 })
