dbcc712ed167a8fc8dd0e94167093884ebd3f825
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-12 17:05:28 +0800

    socket live chat server updated (in learning phase)

--- socketlivechat/src/index.js
+++ socketlivechat/src/index.js
@@ -16,15 +16,50 @@ app.get('/:livechatId', function (req, res) {
 
 })
 
+// temp.. serve the admin frontend files
+app.get('/admintemp/:livechatId', function (req, res) {
+
+    res.render('admin', {
+        livechatId: req.params.livechatId
+    })
+
+})
+
 // server listening on port 80
 server.listen(process.env.PORT, () => {
-    console.log('livechat server listening on port ' + process.env.PORT)
+    console.log('livechat socket server listening on port ' + process.env.PORT)
 })
 
 // update and emit the list of clients' socket id
-var socketClientListUpdate = (socket) => {
-    let allsockets_inroom = io.nsps['/'].adapter.rooms[socket.room]
-    socket.to(socket.room).emit('clientlist_update', { clientSocketsId: allsockets_inroom })
+var socketClientListUpdate = (roomname) => {
+
+    try{
+        let allsocketsinfo = io.nsps['/'].adapter.rooms[roomname]
+
+        if(allsocketsinfo) {
+            // get all the sockets in the room first
+            let allsockets_id = Object.keys(allsocketsinfo.sockets)
+
+            let clientsInfo = []
+            for (let i = 0; i < allsockets_id.length; ++i) {
+
+                let clientSocket = io.sockets.connected[allsockets_id[i]]
+
+                // I only need clients socket
+                if (clientSocket.isClientMah) {
+                    clientsInfo.push({ clientSocketId: allsockets_id[i], clientName: clientSocket.username})
+                }
+
+            }
+
+            // send the online clients list
+            io.to(roomname).emit('clientlist_update', { clientsInfo: clientsInfo })
+        }
+    }
+    catch(e) {
+        console.log(e)
+    }
+
 }
 
 io.on('connection', (socket) => {
@@ -38,24 +73,72 @@ io.on('connection', (socket) => {
 
             // get the rooms info in this socket
             let rooms = Object.keys(socket.rooms)
-            
+
             // store the room name in the socket session for this client
             socket.room = rooms[1]
 
+            // this socket is a client
+            socket.isClientMah = true
+
+            // store the client username
+            socket.username = data.username
+
             // informed that new user has joined the room
-            socketClientListUpdate(socket)
+            socketClientListUpdate(socket.room)
+
+            // confirmation about joining this room
+            socket.emit('client_joined', {socketId: rooms[0]})
         })
 
+    })
+
+    // listening on whether got any admin request to join any room or not
+    socket.on('admin_join_room', (data)=>{
+
+        socket.join(data.roomId, () => {
+
+            // get the rooms info in this socket
+            let rooms = Object.keys(socket.rooms)
+
+            // store the room name in the socket session for this admin
+            socket.room = rooms[1]
+
+            // this socket is an admin
+            socket.isClientMah = false
+
+            // store the admin username
+            socket.username = data.username
+
+            // confirmation about joining this room
+            socket.emit('admin_joined', { socketId: rooms[0] })
+
+            // let the admin know about current list of client online
+            socketClientListUpdate(socket.room)
+
+        })
+
+    })
+
+    // listening on whether admin want to send msg to client or not
+    socket.on('admin_send_client_msg', (data) => {
+        console.log(data)
+        socket.to(data.clientSocketId).emit('client_receiving_msg', { msg: data.msg, adminUsername: data.adminUsername} );
     })
 
     // when the user disconnects
     socket.on('disconnect', () => {
 
-        // update the list of client socket id again
-        socketClientListUpdate(socket)
+        if (socket.isClientMah) {
+            // update the list of client socket id again
+            socketClientListUpdate(socket.room)
 
-        // client officially leave this room
-        socket.leave(socket.room)
+            // client officially leave this room
+            socket.leave(socket.room)
+        }
+        else {
+            // admin officially leave this room
+            socket.leave(socket.room)
+        }
 
     })
 
