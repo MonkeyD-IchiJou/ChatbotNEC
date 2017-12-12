f9641c02ec3fc557b607a1eb8cce7ce025e4ab45
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-12 15:10:38 +0800

    setting up view engine; socket io settings modified

--- socketlivechat/src/index.js
+++ socketlivechat/src/index.js
@@ -1,33 +1,62 @@
 var app = require('express')()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
+const path = require('path')
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // serve the client files
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/build/index.html')
+app.get('/:livechatId', function (req, res) {
+
+    res.render('index', {
+        livechatId: req.params.livechatId
+    })
+
 })
 
+// server listening on port 80
 server.listen(process.env.PORT, () => {
     console.log('livechat server listening on port ' + process.env.PORT)
 })
 
-// each live chat projects is one room
-// room name will be the UUID of the livechat project
-var totalrooms = ['1', '2', '3']
+// update and emit the list of clients' socket id
+var socketClientListUpdate = (socket) => {
+    let allsockets_inroom = io.nsps['/'].adapter.rooms[socket.room]
+    socket.to(socket.room).emit('clientlist_update', { clientSocketsId: allsockets_inroom })
+}
 
 io.on('connection', (socket) => {
 
-    console.log('a user has connected, checked which room this user is supposed to be in?')
+    // listening on whether got any new clients request to join any room or not
+    // each live chat projects is one room
+    // room name will be the UUID of the livechat project
+    socket.on('client_join_room', (data) => {
 
-    let roomname = 'm8nySihGhfPK37Wj5dTNkUHSnkMdaVjVJjFGvZMp462pbqJAB'
+        socket.join(data.roomId, () => {
 
-    socket.join(roomname, () => {
-        let rooms = Object.keys(socket.rooms)
-        console.log(rooms) // [ 'room name', 'socket id' ]
+            // get the rooms info in this socket
+            let rooms = Object.keys(socket.rooms)
+            
+            // store the room name in the socket session for this client
+            socket.room = rooms[1]
+
+            // informed that new user has joined the room
+            socketClientListUpdate(socket)
+        })
+
     })
 
-    io.emit('listenpls', 'hello ' + socket.id)
+    // when the user disconnects
+    socket.on('disconnect', () => {
 
-    socket.to(roomname).emit('an event', { some: 'data' })
+        // update the list of client socket id again
+        socketClientListUpdate(socket)
+
+        // client officially leave this room
+        socket.leave(socket.room)
+
+    })
 
 })
