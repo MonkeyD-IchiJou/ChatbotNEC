03d3dec8a6581d76829d5ba2f746c389b1e56080
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-17 17:27:37 +0800

    create a new post request to get the user info in the db

--- userapi/src/routes/v1/user.js
+++ userapi/src/routes/v1/user.js
@@ -8,9 +8,57 @@ const { matchedData, sanitize } = require('express-validator/filter')
 process.env.MYSQL_DATABASE = 'NECAIDB'
 process.env.MYSQL_USER = 'necaidbuser'
 process.env.MYSQL_PASSWORD = 'NECAIDBuser20171020'
-process.env.jwtSecret = 'soseCREToMg8228'*/
-
+process.env.jwtSecret = 'soseCREToMg8228'
+*/
 var { Database } = require('../../database')
+
+// get the live chat info based on this uuid
+var getUserInfo = (user_id) => {
+
+    return new Promise(async (resolve, reject) => {
+
+        // connect to mariadb/mysql
+        let database = new Database()
+
+        try {
+            // all necessary sql queries
+            const sql_queries = [
+                'SELECT * FROM users WHERE id=?'
+            ]
+
+            // all possible errors
+            const db_errors = [
+                'no such registered user in db'
+            ]
+
+            // delete this intent
+            let row_user = await database.query(sql_queries[0], [user_id])
+
+            if (!row_user[0]) {
+                throw db_errors[0]
+            }
+
+            // return the user info
+            let userinfo = {
+                id: row_user[0].id,
+                email: row_user[0].email,
+                username: row_user[0].username,
+                joindate: row_user[0].joindate
+            }
+            resolve(userinfo)
+
+        }
+        catch (e) {
+            // reject the error
+            reject(e.toString())
+        }
+
+        // rmb to close the db
+        let dbclose = await database.close()
+
+    })
+
+}
 
 // every api router will go through JWT verification first
 router.use(
@@ -48,8 +96,18 @@ router.use(
     }
 )
 
-router.get('/', (req, res) => {
-    res.json({success: 'true'})
+router.post('/', (req, res) => {
+
+    getUserInfo(req.decoded.data.i).then((userinfo) => {
+
+        // send the result back to client
+        res.setHeader('Content-type', 'application/json')
+        res.send(JSON.stringify({ success: true, userinfo: userinfo }))
+
+    }).catch((error) => {
+        return res.status(422).json({ success: false, errors: error })
+    })
+
 })
 
 module.exports = router
