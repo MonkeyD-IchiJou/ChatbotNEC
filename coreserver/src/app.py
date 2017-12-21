627a079bda0fb58506563c25261fd41ba3bf8027
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-21 16:57:47 +0800

    json convert to yml file and then send it for dialogue training

--- coreserver/src/app.py
+++ coreserver/src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import json
+import yaml
 
 # init the flask app
 app = Flask(__name__)
@@ -10,9 +11,15 @@ def query():
     return jsonify(success=True, response=request.get_json()['projectName'])
 
 
-@app.route('/something')
-def something():
-    return jsonify(success=True)
+@app.route('/train', methods=['POST'])
+def train():
+    # this is how i train my dialogue
+    #additional_arguments = {"epochs": 300}
+    #rsTrain.train_dialogue_model("moodbot/domain.yml", "moodbot/data/stories.md", "moodbot/models/dialogue", False, None, additional_arguments)
+    ff = open('meta.yml', 'w+')
+    yaml.dump(request.get_json(), ff,default_flow_style=False)
+
+    return jsonify(success = True)
 
 
 # run my flask app
