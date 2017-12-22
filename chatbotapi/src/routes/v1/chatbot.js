5a8b535ee9c83530603f5accaec2d97b588d1594
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-22 18:14:03 +0800

    mongodb first test and json2md first test

--- chatbotapi/src/routes/v1/chatbot.js
+++ chatbotapi/src/routes/v1/chatbot.js
@@ -4,15 +4,146 @@ const { check, validationResult } = require('express-validator/check')
 const { matchedData, sanitize } = require('express-validator/filter')
 const uuidv4 = require('uuid/v4')
 const bs58 = require('bs58')
+const MongoClient = require('mongodb').MongoClient
+const json2md = require("json2md")
+
+json2md.converters.storyname = function (input, json2md) {
+    return "## " + input
+}
+
+json2md.converters.asterisks = function (input, json2md) {
+    let output = ''
+    for(let i = 0; i < input.length; ++i) {
+        if(typeof input[i] == 'string') {
+            output += "* " + input[i] + "\n"
+        }
+        else {
+            let j2md = json2md([
+                input[i]
+            ])
+            output += j2md
+        }
+    }
+    return output
+}
+
+json2md.converters.actions = function (input, json2md) {
+    let output = ''
+    for (let i = 0; i < input.length; ++i) {
+        if (typeof input[i] == 'string') {
+            output += "  - " + input[i] + "\n"
+        }
+    }
+    return output
+}
+
+
+let j2md = json2md([
+    {
+        storyname: "happy path"
+    },
+    {
+        asterisks: [
+            '_greet',
+            {
+                actions: [
+                    "utter_greet"
+                ]
+            },
+            '_mood_great',
+            {
+                actions: [
+                    "utter_happy"
+                ]
+            }
+        ]
+    },
+    {
+        storyname: "sad path 1"
+    },
+    {
+        asterisks: [
+            "_greet",
+            {
+                actions: [
+                    "utter_greet"
+                ]
+            },
+            "_mood_unhappy",
+            {
+                actions: [
+                    "utter_cheer_up",
+                    "utter_did_that_help"
+                ]
+            },
+            "_mood_affirm",
+            {
+                actions: [
+                    "utter_happy"
+                ]
+            }
+        ]
+    }
+    
+])
+
+j2md
 
 // temp only.. remove it in production
 /*process.env.MASQL_HOST = 'localhost'
+process.env.MAMONGO_HOST = 'localhost'
 process.env.MYSQL_DATABASE = 'NECAIDB'
 process.env.MYSQL_USER = 'necaidbuser'
 process.env.MYSQL_PASSWORD = 'NECAIDBuser20171020'
 process.env.jwtSecret = 'soseCREToMg8228'*/
 
 var { Database } = require('../../database')
+
+// MongoDB Connection URL
+const url = 'mongodb://' + process.env.MAMONGO_HOST
+
+var mongodb_example = () => {
+
+    return new Promise(async (resolve, reject) => {
+
+        let client = ''
+
+        try {
+            // connect to my mongodb
+            client = await MongoClient.connect(url)
+
+            // connect to my db
+            const db = client.db(process.env.MYSQL_DATABASE)
+
+            // Get the collection from my db
+            const collection = db.collection('domain')
+
+            // Insert some documents
+            //let ins = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+            //ins
+
+            // find all documents
+            let findall = await collection.find({}).toArray()
+            findall
+
+            resolve(findall)
+
+        } catch (e) {
+            // reject the error
+            reject(e.toString())
+        }
+
+        // rmb to close my mongodb collection
+        client.close()
+    })
+
+}
+
+/*mongodb_example().then((r)=>{
+    console.log(r)
+}).catch((e)=>{
+    console.log(e)
+})*/
 
 // every api router will go through JWT verification first
 router.use(
