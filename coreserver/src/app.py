aa22d54fbcf3154f06a1011800e687ad23a7b508
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-26 17:57:27 +0800

    receive domain json and turn it into domain.yml; receive stories string and put it into a file too; and then training coming

--- coreserver/src/app.py
+++ coreserver/src/app.py
@@ -11,15 +11,22 @@ def query():
     return jsonify(success=True, response=request.get_json()['projectName'])
 
 
-@app.route('/train', methods=['POST'])
-def train():
+@app.route('/training', methods=['POST'])
+def training():
+    # turn domain into yml format file
+    domainfile = open('domain.yml', 'w+')
+    yaml.dump(request.get_json()['domain'], domainfile, default_flow_style=False)
+
+    # turn stories into a file
+    storiesfile = open('stories.md', 'w+')
+    storiesfile.write(request.get_json()['stories'])
+    storiesfile.close()
+
     # this is how i train my dialogue
     #additional_arguments = {"epochs": 300}
     #rsTrain.train_dialogue_model("moodbot/domain.yml", "moodbot/data/stories.md", "moodbot/models/dialogue", False, None, additional_arguments)
-    ff = open('meta.yml', 'w+')
-    yaml.dump(request.get_json(), ff,default_flow_style=False)
 
-    return jsonify(success = True)
+    return jsonify(success=True)
 
 
 # run my flask app
