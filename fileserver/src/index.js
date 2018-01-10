321c76407bdf8dbeeeec3d9ff39cebfbebf2ef81
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-10 11:26:52 +0800

    manipulating files in my server

--- fileserver/src/index.js
+++ fileserver/src/index.js
@@ -2,6 +2,7 @@ var app = require('express')()
 const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser')
 var cors = require('cors')
+var fs = require('fs')
 
 // default options
 app.use(fileUpload())
@@ -17,18 +18,49 @@ app.get('/', (req, res) => {
 
 app.post('/upload', function (req, res) {
     if (!req.files)
-        return res.status(400).send('No files were uploaded.');
+        return res.status(400).send('No files were uploaded.')
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let sampleFile = req.files.sampleFile;
+    let sampleFile = req.files.sampleFile
 
     // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv('/home/node/app/src/static/' + sampleFile.name, function (err) {
+    sampleFile.mv('/home/node/app/static/' + sampleFile.name, function (err) {
         if (err)
-            return res.status(500).send(err);
+            return res.status(500).send(err)
 
-        res.send('File uploaded!');
+        res.send('File uploaded!')
     });
+})
+
+app.get('/infos', (req, res)=>{
+
+    let path = '/home/node/app/static'
+    let returndata = []
+
+    let items = fs.readdirSync(path)
+
+    for (var i = 0; i < items.length; i++) {
+
+        var file = path + '/' + items[i]
+        let stats = fs.statSync(file)
+
+        let size = stats["size"]
+        var convertsize = Math.floor(Math.log(size) / Math.log(1024))
+        let finalsize = (size / Math.pow(1024, convertsize)).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][convertsize]
+
+        returndata.push({ name: items[i], size: finalsize })
+
+    }
+
+    res.json(returndata)
+
+})
+
+app.delete('/remove', (req, res)=>{
+    var filePath = '/home/node/app/static'
+    fs.unlinkSync(filePath + '/' + req.query.filename)
+    res.json('removed')
+
 })
 
 app.listen(process.env.PORT, () => {
