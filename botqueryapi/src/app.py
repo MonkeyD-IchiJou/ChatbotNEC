44cfff67d32fe3b73a894f6e0c64c0ddfac3b070
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-21 16:54:12 +0800

    botqueryapi updated implementation; new conversations and querycontinue routes are added; optimised

--- botqueryapi/src/app.py
+++ botqueryapi/src/app.py
@@ -1,14 +1,53 @@
 from flask import Flask, request, jsonify
 import json
-import rasa_core.train as rsTrain
+
+from rasa_core.actions import Action
+from rasa_core.agent import Agent
+from rasa_core.interpreter import RasaNLUInterpreter
 
 # init the flask app
 app = Flask(__name__)
+allagents = {}
+
+# this is how i run my agent
+def run(nlu_model_path, dialogue_model_path):
+    interpreter = RasaNLUInterpreter(nlu_model_path)
+    agent = Agent.load(dialogue_model_path, interpreter=interpreter)
+    return agent
 
 
-@app.route('/training', methods=['POST'])
-def training():
-    return jsonify(status='ready')
+@app.route('/conversations', methods=['POST'])
+def conversations():
+    global allagents
+
+    projectName = request.get_json()['projectName']
+
+    if projectName in allagents:
+        # this agent alr exist in the allagents
+        return jsonify(allagents[projectName].start_message_handling(text_message=request.get_json()['text_message'], sender_id=request.get_json()['sender_id']))
+
+    # if allagents haven't run this project yet
+    # run this agent and store it
+    allagents[projectName] = run(
+        "/nluprojects/" + projectName + "/model",
+        "/app/dialogues/" + projectName
+    )
+    return jsonify(allagents[projectName].start_message_handling(text_message=request.get_json()['text_message'], sender_id=request.get_json()['sender_id']))
+
+
+@app.route('/querycontinue', methods=['POST'])
+def querycontinue():
+    global allagents
+
+    projectName = request.get_json()['projectName']
+
+    if projectName in allagents:
+        # this agent alr exist in the allagents
+        return jsonify(allagents[projectName].continue_message_handling(sender_id=request.get_json()['sender_id'], executed_action=request.get_json()['executed_action'], events=[]))
+
+    # if this agent for some reason is not loaded yet...
+    return jsonify(success='false', result='agent is not available')
+
 
 
 # run my flask app
